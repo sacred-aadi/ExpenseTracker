e7d489886885bd3cfb3bb39f7c31c0d216ab1c7d
sacred.aadi <amheisenbergsprinciple@gmail.com>
2026-07-09 13:43:48 +0530

    Updated expense tracker

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-a = 10;
-print(a)
+print("===== Welcome to the Expense Tracker!=====")
+option = int(input(" 1. to add expense.\n 2. to view expense\n 3. to exit\nEnter your option: "))
+
+if option == 1:
+    print("You have selected to add an expense.")
+elif option == 2:
+    print("You have selected to view expenses.")
+elif option == 3:
+    print("Exiting the Expense Tracker. Goodbye!")
+else :
+    print("Invalid option selected. Please try again.")
+
+expenses = []
+match option:
+    case 1:
+        
+       
